9daf1b85df18d84f8d76d569e83bb4bb93129e0b
SNderi <79838849+SNderi@users.noreply.github.com>
2022-04-19 11:16:17 +0300

    Printf emulator, header file, formatting functions

---
+++ mainh.h
@@ -0,0 +1,23 @@
+#ifndef MAINH_H
+#define MAINH_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * struct spic - Structure for identifier types and %
+ * @dt: Datatype or %
+ * @f: Associated function
+ */
+typedef struct spic
+{
+	char *dt;
+	int (*f)(va_list);
+} spic_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list);
+int print_s(va_list);
+int print_per(va_list);
+int print_di(va_list);
+#endif /*MAINH_H*/
