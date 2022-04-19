3d9c055c59312dc20c67524fd9d1073e31ec3bb3
Vincent-ondeng <vincentondeng@gmail.com>
2022-04-19 12:48:28 -0700

    putchar

--- _putchar.c
+++ _putchar.c
@@ -4,7 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1, On error -1
+ * Return: Number of characters written
  */
 int _putchar(char c)
 {
